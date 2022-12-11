dd63090f804ae107d66201e7355eb3222eafcdde
Georgi Nikov <nikov.george@gmail.com>
2022-12-11 19:53:55 +0200

    Create reverse.js

---
+++ Arrays/lab/reverse.js
@@ -0,0 +1,12 @@
+function reverse (n, inputArr) {
+let arr = [];
+for (let i = 0; i<n i++){
+
+}
+let output = '';
+for(let i = arr.length-1; i>0; i--){
+
+}
+console.log(output);
+}
+reverse(4,[-1,20,99,5])
