59e33660d16bc7e0cda07883cd77949cb7a3778b
Georgi Nikov <nikov.george@gmail.com>
2022-12-12 21:58:13 +0200

    Update reverse.js

--- Arrays/lab/reverse.js
+++ Arrays/lab/reverse.js
@@ -1,12 +1,13 @@
-function reverse (n, inputArr) {
-let arr = [];
-for (let i = 0; i<n i++){
+function reverseArray (n, inputArr) {
 
+let arr = [];
+for (let i = 0; i < n; i++) {
+arr.push(inputArr);
 }
 let output = '';
-for(let i = arr.length-1; i>0; i--){
-
+for (let i = arr.length-1; i>=0; i--) {
+output+= arr;
 }
 console.log(output);
 }
-reverse(4,[-1,20,99,5])
+reverseArray([3,[10, 20, 30, 40, 50]])
