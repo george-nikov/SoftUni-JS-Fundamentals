c999e9536ee0aac1a0fb8c35e426d97b7be8f6e2
Georgi Nikov <nikov.george@gmail.com>
2022-12-23 12:18:33 +0200

    Create nxnMatrix.js

---
+++ Functions/exercises/nxnMatrix.js
@@ -0,0 +1,14 @@
+function nxnMatrix (a) {
+
+    let p = '';
+
+    for (let j = 1; j <= a; j++){
+        for (let i = 1; i <= a; i++) {
+            p += a + ' ';
+             
+        }
+        console.log(p);
+        p = '';
+    }
+}
+nxnMatrix([2])
