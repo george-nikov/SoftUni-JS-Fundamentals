a7a00f1a226e199701286dbbe75d6908bbbb31c5
Georgi Nikov <nikov.george@gmail.com>
2023-01-23 13:32:41 +0200

    Update cone.js

--- Data Types and Variables/more exercises/cone.js
+++ Data Types and Variables/more exercises/cone.js
@@ -1,9 +1,9 @@
 function cone (radius, height) {
 
 let volume = Number(1/3*Math.PI* (radius*radius)*height);
-let area = Number(Math.PI*(radius*radius)+Math.PI*radius*Math.SQRT2*radius*radius+height*height);
-console.log(volume);
-console.log(area);
+let area = Number(Math.PI * radius*(radius+Math.sqrt(radius*radius+height*height)));
+console.log(`volume = ${volume.toFixed(4)}`);
+console.log(`area = ${area.toFixed(4)}`);
 
 }
 cone([3,5])
