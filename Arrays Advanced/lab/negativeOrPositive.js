24b686b57ea7f757644976e50014ebc76ab2c169
Georgi Nikov <nikov.george@gmail.com>
2023-02-13 18:39:55 +0200

    Update negativeOrPositive.js

--- Arrays Advanced/lab/negativeOrPositive.js
+++ Arrays Advanced/lab/negativeOrPositive.js
@@ -1,17 +1,17 @@
 function negativePositive (arr) {
 
-    let result = [];
-
-for (let i = 0; i < arr.length; i++) {
-
-    if (arr[i] < 0) {
-        result.unshift(arr[i]);
-    }
-    else {
-        result.push(arr[i]);
+    let final = [];
+    let current;
+    
+    for (let i = 0; i < arr.length; i++) {
+        current = arr[i];
+        if (current < 0) {
+            final.unshift(current);
         }
+        else if (current >=0) {
+            final.push(current)
+        }
+    }
+    console.log(final.join('\n'));
 }
-console.log(result.join('\n'));
-
-}
-negativePositive(['7','-2','8','9'])
+negativePositive(['7', '-2', '8', '9'])
