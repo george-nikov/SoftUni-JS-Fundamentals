749ec4b3124979cfc2508314bd2b92ce100cad96
Georgi Nikov <nikov.george@gmail.com>
2023-02-13 18:31:22 +0200

    Update firstLast.js

--- Arrays Advanced/lab/firstLast.js
+++ Arrays Advanced/lab/firstLast.js
@@ -1,9 +1,10 @@
-function firstLast (input) {
+function firstLast (arr) {
 
-let a = Number(input[0]);
-let b = Number(input[input.length-1]);
-let sum = a + b;
-console.log(sum);
+    let last = Number(arr.pop());
+    let first = Number(arr.shift());
+    let sum = Number(last + first);
+
+    console.log(sum);
 
 }
 firstLast(['20', '30', '40'])
